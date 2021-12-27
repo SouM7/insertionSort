66076e4cd05a82e95eb504ee1a3e6a00db4ee042
SouM7 <soumia.bouhaouche@gmail.com>
2021-12-27 14:35:21 +0100

    insertion sort algo

---
+++ insertionSort.js
@@ -0,0 +1,31 @@
+function insertionSort(inputArr) {
+
+    let n = inputArr.length;
+
+        for (let i = 1; i < n; i++) {
+
+            // Choosing the first element in our unsorted subarray
+
+            let current = inputArr[i];
+
+            // The last element of our sorted subarray
+
+            let j = i-1; 
+
+            while ((j > -1) && (current < inputArr[j])) {
+
+                inputArr[j+1] = inputArr[j];
+                j--;
+
+            }
+
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
+
+let inputArr = [5, 1, 10, 6, 4, 13,7];
+
+insertionSort(inputArr);
+
+console.log(inputArr);
